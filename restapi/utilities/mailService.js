c6300f8cc7c8a7ffc2ed9ce4e34f843811a718fd
ankit <ankit.khandelwal@echelonedge.com>
2022-08-26 12:43:31 +0530

    changes

--- restapi/utilities/mailService.js
+++ restapi/utilities/mailService.js
@@ -25,104 +25,76 @@ let mail = {};
 
 
 
-function sendEmail() {
-    console.log("inside send mail");
-    var smtpTransport = nodemailer.createTransport({
-        // service: "Gmail",
-        // host: "smtp-mail.outlook.com",
-        host: "smtp.office365.com",
-        port: 587,
-        // secure: false,
-        secureConnection: false,
-        STARTTLS: true,
-        auth: {
-     user: "ankit.khandelwal@echelonedge.com",
-            pass: "svjvkmfflpfffjqx"
+// function sendEmail() {
+//     console.log("inside send mail");
+//     var smtpTransport = nodemailer.createTransport({
+//         // service: "Gmail",
+//         // host: "smtp-mail.outlook.com",
+//         host: "smtp.office365.com",
+//         port: 587,
+//         // secure: false,
+//         secureConnection: false,
+//         STARTTLS: true,
+//         auth: {
+//      user: "ankit.khandelwal@echelonedge.com",
+//             pass: "svjvkmfflpfffjqx"
 
-        },
-  
-    });
-    var mailOptions = {
-        from: "ankit.khandelwal@echelonedge.com",
-        to: "ashish.kumar@echelonedge.com",
-      
-        subject: "test",
-       
-        text: "this is the test email with outlook",
-     
-    };
-    smtpTransport.sendMail(mailOptions, function (error, response) {
-        console.log("error======", error, "\nMessage sent: " + response);
-        if (error) {
-          
-        } else {
-            console.log("Message sent: " + response.message);
-        
-        }
-    });
-}
-
-
-
-
-function test_mail() {
-    let transporter = nodemailer.createTransport({
-        service: "smtp.office365.com",
-        port: 587,
-        startls: true,
-        auth: {
-            user: "ankit.khandelwal@echelonedge.com",
-            pass: "svjvkmfflpfffjqx"
-
-        },
-    });
-
-    transporter.sendMail({
-        from: 'ankit.khandelwal@echelonedge.com',
-        to: 'ankit.khandelwal@echelonedge.com',
-        // cc: options.cc,
-        subject: "options.subject",
-        html: "  my test mail",
-        // attachDataUrls: true,
-        // attachments: options.attachments
-    },
-        function (error, msg) {
-            console.log(error)
-            console.log(msg)
-        }
-    )
-}
-
-
-sendEmail();
+//         },
+//   
+//     });
+//     var mailOptions = {
+//         from: "ankit.khandelwal@echelonedge.com",
+//         to: "ashish.kumar@echelonedge.com",
+//       
+//         subject: "test",
+//        
+//         text: "this is the test email with outlook",
+//      
+//     };
+//     smtpTransport.sendMail(mailOptions, function (error, response) {
+//         console.log("error======", error, "\nMessage sent: " + response);
+//         if (error) {
+//           
+//         } else {
+//             console.log("Message sent: " + response.message);
+//         
+//         }
+//     });
+// }
 
 
 
 
 
-return
+
+
+// sendEmail();
+
+
+
+
+
+
 mail.sendEmail = (email, options) => {
     return new Promise((resolve, reject) => {
         try {
-            let transporter = nodemailer.createTransport({
-                service: "smtp.office365.com",
-                port: 587,
-                startls: true,
-                auth: {
-                    user: "ankit.khandelwal@echelonedge.com",
-                    pass: "svjvkmfflpfffjqx"
-
-                },
-            });
-            // let transporter = nodemailer.createTransport({
-            //     service: 'gmail',
-            //     auth: {
-            //         user: 'paypalshopping2021@gmail.com',
-            //         pass: 'test123@A'
-            //     }
-            // });
+            var smtpTransport = nodemailer.createTransport({
+                        // service: "Gmail",
+                        // host: "smtp-mail.outlook.com",
+                        host: "smtp.office365.com",
+                        port: 587,
+                        // secure: false,
+                        secureConnection: false,
+                        STARTTLS: true,
+                        auth: {
+                     user: "ankit.khandelwal@echelonedge.com",
+                            pass: "svjvkmfflpfffjqx"
+                
+                        },
+                  
+                    });
             ejs.renderFile("views/" + options.template, options).then((resFile) => {
-                transporter.sendMail({
+                smtpTransport.sendMail({
                     from: 'ankit.khandelwal@echelonedge.com',
                     to: email,
                     cc: options.cc,
